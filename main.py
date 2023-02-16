d29584650e043121f4a0e834fd96f8c815036441
Catalin Suceveanu <79462999+catalinsuceveanu@users.noreply.github.com>
2023-02-16 17:28:58 +0200

    Main cleanup before start of work

---
+++ main.py
@@ -0,0 +1,23 @@
+from Week1_homework.round import Round
+
+
+def print_curr_game_state(curr_round):
+    print(
+        f"You have {curr_round.get_no_of_tries_left()} tries left.\n",
+        f"Used letters: {curr_round.list_already_tried_letters_in_natural_language()}\n",
+        f"Word: {curr_round.get_incomplete_word()}"
+    )
+    letter = input("Guess a letter: ")
+    print("\n")
+    return letter
+
+
+def main():
+    curr_round = Round()
+    while curr_round.get_no_of_tries_left() > 0:
+        letter = print_curr_game_state(curr_round)
+        curr_round.add_letter_to_already_tried_letters(letter)
+
+
+if __name__ == '__main__':
+    main()
--- round.py
+++ round.py
@@ -51,23 +51,3 @@ class Round:
         return natural_language_letters_list
 
 
-def print_curr_game_state(curr_round):
-    print(
-        f"You have {curr_round.get_no_of_tries_left()} tries left.\n",
-        f"Used letters: {curr_round.list_already_tried_letters_in_natural_language()}\n",
-        f"Word: {curr_round.get_incomplete_word()}"
-    )
-    letter = input("Guess a letter: ")
-    print("\n")
-    return letter
-
-
-def main():
-    curr_round = Round()
-    while curr_round.get_no_of_tries_left() > 0:
-        letter = print_curr_game_state(curr_round)
-        curr_round.add_letter_to_already_tried_letters(letter)
-
-
-if __name__ == '__main__':
-    main()
