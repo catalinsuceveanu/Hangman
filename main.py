7cc5c4ec2fbd75ffa7b936f622b7712e9f1977b1
Catalin Suceveanu <79462999+catalinsuceveanu@users.noreply.github.com>
2023-02-18 15:01:13 +0200

    GBB-3 Round class has been created and has full functionality. I am also pushing the modifications made in main for you to be able to test functionalities of the class easily.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from round import Round
 
 def print_curr_game_state(curr_round):
     print(
-        f"You have {curr_round.get_no_of_tries_left()} tries left.\n",
+        f"You have {curr_round.get_guesses_left()} tries left.\n",
         f"Used letters: {curr_round.list_already_tried_letters_in_natural_language()}\n",
         f"Word: {curr_round.get_incomplete_word()}"
     )
@@ -13,11 +13,16 @@ def print_curr_game_state(curr_round):
 
 
 def main():
-    curr_round = Round()
-    while curr_round.get_no_of_tries_left() > 0:
+    list_of_words = ["asccaasdfgggha", "asdfddddg", "afrgffffrg"]
+
+    curr_round = Round(list_of_words)
+    print(curr_round.get_the_word())
+    print(list_of_words)
+
+    while curr_round.get_guesses_left() > 0:
         letter = print_curr_game_state(curr_round)
         if is_valid(letter):
-            curr_round.add_letter_to_already_tried_letters(letter.lower())
+            curr_round.play_a_round(letter.lower())
 
 
 if __name__ == '__main__':
